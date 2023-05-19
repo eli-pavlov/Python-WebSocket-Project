8412a5e381fe51d927ba6a4591d40e46f38ebc3b
eli-pavlov <pavlov.eli@gmail.com>
2023-05-19 16:15:13 +0300

    Python project

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def last_date():
     current_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
     return str(current_date)
 print(last_date())
+current_id = 0
+alarm1 = 0
+alarm2 = 0
 
 
 # define and create database if not exists:
@@ -29,9 +32,13 @@ SELECT rowid, *
 FROM station_status;
 """
 
-sql_insert_message = """
-INSERT INTO station_status VALUES
-(?, ?, ?, ?);
+sql_insert_message = f"""
+INSERT INTO station_status ({current_id}, {alarm1}, {alarm2}, {last_date})
+SELECT {current_id}
+WHERE NOT EXISTS (SELECT 1 FROM station_status WHERE station_id={current_id});
+UPDATE station_status
+SET alarm1={alarm1}, alarm2={alarm2}, last_date={last_date}
+WHERE station_id={current_id};
 """
 
 with sql.connect(db_file) as conn:
@@ -68,12 +75,21 @@ while True:
             # Incoming data from a client
             data = sock.recv(1024).decode()
             if data:
+                data = eval(data)
                 print('Received data:', data)
+                current_id = int(data[0])
+                alarm1 = int(data[1])
+                alarm2 = int(data[2])
+                print(current_id)
+                print(alarm1)
+                print(alarm2)
                 # Enter received data to database:
                 with sql.connect(db_file) as conn:
-                    conn.execute(sql_insert_message, (station_id, alarm1, alarm2, last_date))
+                    conn.execute(sql_insert_message)
+                    conn.commit()
                 # Return reply to the client
-                sock.sendall(data.encode())
+                response = " Station status received"
+                sock.sendall(response.encode())
             else:
                 # Client has closed the connection
                 print('Client disconnected:', sock.getpeername())
